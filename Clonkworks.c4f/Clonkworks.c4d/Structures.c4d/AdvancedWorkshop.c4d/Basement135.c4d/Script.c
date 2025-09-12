f73d70aedbb23fae82ce36dc2e76f458bc371d03
CodenTheSynth <watemborskae@gmail.com>
2025-09-12 15:22:36 +0200

    Added a basic not-very functional Advanced Workshop. it uses normal workshop logic for now.

---
+++ Clonkworks.c4f/Clonkworks.c4d/Structures.c4d/AdvancedWorkshop.c4d/Basement135.c4d/Script.c
@@ -0,0 +1,10 @@
+/*-- Fundament --*/
+
+/* Alle Fundamente leiten ihre Grundfunktionalität von BAS7 ab */
+
+#strict
+
+#include BAS7
+
+private func BasementID() { return(BAS3); }
+private func BasementWidth() { return(135); }
