70973f3edc013f75e1e83ba999ca389d0581cd8a
CodenTheSynth <watemborskae@gmail.com>
2025-09-19 21:21:42 +0200

    Better steam jump physics

--- Clonkworks.c4f/Clonkworks.c4d/Items.c4d/Equipment.c4d/Steam-Jump.c4d/Script.c
+++ Clonkworks.c4f/Clonkworks.c4d/Items.c4d/Equipment.c4d/Steam-Jump.c4d/Script.c
@@ -24,9 +24,29 @@ protected func DischargeAir(){
 	var vX =GetXDir(Contained());
 	var vY =GetYDir(Contained());
 	
-	SetXDir(vX * 4, Contained());
-	SetYDir(vY * 4, Contained());
-	Contained()->SetAction("Tumble");
+	var nvX = vX * 3;
+	var nvY = vY * 3;
+	
+	if(Abs(vX) < 50)
+	if(nvX > 0)
+	nvX = Min(50, nvX);
+	else
+	nvX = Max(-50, nvX);;
+
+	if(Abs(vY) < 50)
+	if(nvY > 0)
+	nvY = Min(50, nvY);
+	else
+	nvY = Max(-50, nvY);;
+
+	if(vX > 50) nvX = vX + 50;
+	if(vX < -50) nvX = vX - 50;
+	if(vY > 50) nvY = vY + 50;
+	if(vY < -50) nvY = vY - 50;
+	
+	SetXDir(nvX, Contained());
+	SetYDir(nvY, Contained());
+	//Contained()->SetAction("Tumble");
 	
 	for(var i = 0; i < RandomX(5,10); i++){
 		CreateParticle("Steam", 0,0,RandomX(-vX,0),RandomX(-vY,0),RandomX(50,170),RGBa(255,255,255));
@@ -49,6 +69,7 @@ protected func ChargeAir(){
 }
 
 public func Activate(){
+	[$Launch$]
 	return DischargeAir();
 }
 
