0617d60e674df0e433413cf7f92be22676345f62
CodenTheSynth <watemborskae@gmail.com>
2025-09-20 10:21:03 +0200

    Changed the funnel kit into an advanced line kit, and added a custom linetype system

---
+++ Clonkworks.c4f/Clonkworks.c4d/Items.c4d/Equipment.c4d/AdvancedLineKit.c4d/Script.c
@@ -0,0 +1,112 @@
+/*-- Leitungsbausatz --*/
+
+#strict
+
+//objects that only support custom lines should have the unused line connect type set. LineConnect=128
+
+protected func Hit()
+{
+  Sound("RockHit*");
+}
+
+protected func Activate(clonk)
+{
+  [$TxtConnectline$]
+  var obj = FindObject(0, 1,0,0,0, OCF_LineConstruct(), 0,0,0, obj);
+  if(!obj) return(Message("$TxtNoNewLine$", this()) );
+  var pLine = FindObject(0, 0,0,0,0, 0, "Connect", this() );
+  SetComDir(COMD_Stop(), clonk);
+
+  if(pLine)
+  {
+    if(obj == GetActionTarget(0, pLine) || obj == GetActionTarget(1, pLine) )
+    {
+      // Leitung wieder entfernen
+      Message("$TxtLineRemoval$", this(), GetName(pLine) );
+      RemoveObject(pLine);
+      Sound("Connect");
+      return(1);
+    }
+    else
+    {
+      if(!ConnectLine(pLine, obj) ) return(1);
+    }
+  }
+  else
+  {
+    LineSelection(obj, clonk);
+  }
+
+  return(1);
+}
+
+public func LineSelection(pObject,clonk){
+	if(!pObject->~ALKConnectType()){
+	  Sound("Error");
+      return(Message("$TxtNoNewLine$", this()) );
+	}
+	var linelist = pObject->~ALKConnectType();
+	if(GetLength(linelist) == 1){
+		CreateLine(linelist[0], GetOwner(), pObject, this());
+	}else{
+		CreateMenu(FNKT,clonk,this(),4,"$TxtNoNewLine$");
+		for(var i in linelist){
+			AddMenuItem("$TxtConnectline$: %s", "LineSelectionDone",i,clonk,0,pObject,GetDesc(,i));
+		}
+	}
+}
+
+protected func LineSelectionDone(linetype, from){
+  var obj = FindObject(0, 1,0,0,0, OCF_LineConstruct(), 0,0,0, obj);
+  if(!obj) return(Message("$TxtNoNewLine$", this()));
+  if(obj != from) return(Message("$TxtNoNewLine$", this()));
+  
+  CreateLine(linetype, GetOwner(), from, this());
+}
+
+private func CreateLine(linetype, owner, from, to)
+{
+  var line = CreateObject(linetype, 0, 0, owner);
+  ObjectSetAction(line, "Connect", from, to);
+  Message("$TxtNewLine$", this(), GetName(line) );
+  Sound("Connect");
+  return(line);
+}
+
+/* Verbinden zweier Gebäude mit einer Leitung */
+private func ConnectLine(line, to)
+{
+
+  var line_accept = GetOCF(to) & OCF_Container;
+
+  var t1 = GetActionTarget(0, line);
+  var t2 = to;
+  if(t1 == this() ) { t2 = GetActionTarget(1, line); t1 = to; }
+  
+  var ok = false;
+    if (t1 == to && line_accept)
+      ok = true;
+    if (t2 == to && line_accept)
+      ok = true;
+
+
+  if(!ok)
+  {
+    Sound("Error");
+    Message("$TxtNoConnectType$", this(), GetName(line), GetName(to) );
+    return(0);
+  }
+  
+  ObjectSetAction(line, "Connect", t1, t2);
+
+  Message("$TxtConnect$", Contained(), GetName(line), GetName(to) );
+  Sound("Connect");
+  RemoveObject();
+  return(1);
+}
+
+/* Produkteigenschaften */
+public func IsAnvilProduct() { return(1); }
+public func IsAdvancedProduct() { return(1); }
+public func IsKit() { return(1); }
+func IsMetal(){ return(true); }
---
+++ Clonkworks.c4f/Clonkworks.c4d/System.c4g/ALKstructures.c
@@ -0,0 +1,25 @@
+/*-- Neues Script --*/
+
+#strict
+#appendto CTW0
+#appendto CST1
+#appendto CST2
+#appendto CST3
+#appendto CHEM
+#appendto FNDR
+#appendto HUT1
+#appendto HUT2
+#appendto HUT3
+#appendto IGLO
+#appendto POWR
+#appendto RSRC
+#appendto SAWM
+#appendto WTWR
+#appendto WRKS
+#appendto FORG
+#appendto ADVW
+
+//what advanced lines can be connected?
+public func ALKConnectType(){
+	return([FNPP]);
+}
