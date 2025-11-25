e9055484b5272171d66f9c269fb893cadd470861
CodenTheSynth <watemborskae@gmail.com>
2025-11-25 15:46:31 +0100

    Retired: Short Wires Rule

--- Clonkworks.c4d/System.c4g/AdditionalLineLogic.c
+++ Clonkworks.c4d/System.c4g/AdditionalLineLogic.c
@@ -7,26 +7,6 @@
 #appendto HOSE
 #appendto TRPW
 
-private func Transfer(){
-	if(!ObjectCount(RSWR)) 	return(inherited());
-	if(this()->~CustomLengthLimit()) return(inherited());
-	var from, to;
-	from = GetActionTarget(0);
-	to = GetActionTarget(1);
-	
-	var Distance;
-	Distance = 500;
-	//pipes have a bigger range, so they can be used without pain.
-	if(GetID() != PWRL) Distance = 1200;
-	
-	if(ObjectDistance(from,to) > Distance){
-		LineBreak(false);
-		RemoveObject(this());
-	}
-	
-	return(inherited());
-}
-
 public func isLine(){ return(1); }
 public func KitType(){
 	if(GetID() == SPIP || GetID() == DPIP || GetID() == PWRL){
