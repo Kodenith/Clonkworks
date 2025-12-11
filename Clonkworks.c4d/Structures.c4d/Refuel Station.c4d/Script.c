e053c27c0615d10763231419289d54663bdf33fb
CodenTheSynth <watemborskae@gmail.com>
2025-12-11 17:21:47 +0100

    Fuel Stations can now only hold fuel items.

--- Clonkworks.c4d/Structures.c4d/Refuel Station.c4d/Script.c
+++ Clonkworks.c4d/Structures.c4d/Refuel Station.c4d/Script.c
@@ -13,6 +13,12 @@ private func XLeft(){ return(GetVertex(4,0)); }
 private func XRigt(){ return(GetVertex(5,0)); }
 
 
+//entrance
+func RejectCollect(pObj){
+	if(pObj->~IsFuelItem()) if(ContentsCount() < 25) return(0);
+	return(1);
+}
+
 //grabbing
 func RejectGrabbed(pObj){
 	var iX = AbsX(GetX(pObj));
