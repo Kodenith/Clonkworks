c41337400e4e0e4ce2f7bef364a649c3a34c66a2
CodenTheSynth <watemborskae@gmail.com>
2025-12-21 16:40:29 +0100

    Revamped the whole playground map, this took me a whole day.

---
+++ Clonkworks.c4f/CWP5.c4s/Environment Manager.c4d/SetGoal.c4d/Script.c
@@ -0,0 +1,39 @@
+
+#strict 2
+
+func Initialize(){
+	SetPosition(0,0);
+}
+
+func DoVote(){
+	//return(1);
+	return(IsNetwork());
+}
+
+func Activate(iPlr){
+	var goalList=[GLDM,OREM,ENGI,MELE,MNTK,OILP,WPHT,CI5P,COPM,TITM];
+	var pClonk = GetCursor(iPlr);
+	CreateMenu(GetID(),pClonk,this(),0,GetName());
+	for(var i in goalList){
+	AddMenuItem(GetName(,i),"DoGoal",i,pClonk,0,iPlr);
+	}
+}
+
+func DoGoal(Goal,Plr){
+	if(DoVote()){
+		CastVote(Plr+1,Format("Begin Goal: %s",GetName(,Goal)),"StartGoal(%v)",Goal);
+	}else{
+		StartGoal(Goal);
+	}
+}
+
+global func StartGoal(gl){
+	var g = CreateObject(gl);
+	for(var i in FindObjects(Find_Category(C4D_Goal),Find_Exclude(g))){
+		RemoveObject(i);
+	}
+	for(var i = 0; i < GetPlayerCount(); i++){
+		SetWealth(GetPlayerByIndex(i),125);
+	}
+	Log("Goal Started!");
+}
