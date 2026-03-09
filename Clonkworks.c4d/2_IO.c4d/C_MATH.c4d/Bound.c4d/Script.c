7800a4c1dc1d6c450afab0e821005d7596d18108
Kodenith <watemborskae@gmail.com>
2026-03-09 12:18:04 +0100

    Add Math Bound Circuit

---
+++ Clonkworks.c4d/2_IO.c4d/C_MATH.c4d/Bound.c4d/Script.c
@@ -0,0 +1,39 @@
+/*-- Neues Script --*/
+
+#strict 2
+#include IO__
+
+func Initialize() {
+  SetAction("Attached");
+  SetComDir(COMD_None);
+  return(1);
+}
+
+public func OutputList(){
+  return(["Result"]);
+}
+
+public func InputList(){
+  return(["Number","Min","Max"]);
+}
+
+func MustBeOnWall(){ return(1); }
+
+//used by wire, check if a certain output is active
+public func OutputActive(string OutputName){
+   if(GetType(InputActive("Min")) != C4V_Int && GetType(InputActive("Min")) != 0) return(0);
+   if(GetType(InputActive("Max")) != C4V_Int && GetType(InputActive("Max")) != 0) return(0);
+   if(GetType(InputActive("Number")) != C4V_Int) return(0);
+   return(BoundBy(InputActive("Number"),InputActive("Min"),InputActive("Max")));
+}
+
+func Malfunction(){ OnDetach(); }
+func Damage(){
+  if(GetDamage() > 50) OnDetach();
+}
+
+func CanBeDetached(){ return(1); }
+func OnDetach(){
+  Sound("Connect");
+  Split2Components();
+}
