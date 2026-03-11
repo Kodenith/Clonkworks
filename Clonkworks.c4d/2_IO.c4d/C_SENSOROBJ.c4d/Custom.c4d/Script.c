78c5d6ccb25222678ddb8f8d3cc85f5130387403
Kodenith <watemborskae@gmail.com>
2026-03-11 08:35:36 +0100

    Custom Sensor Now shows what it detects

--- Clonkworks.c4d/2_IO.c4d/C_SENSOROBJ.c4d/Custom.c4d/Script.c
+++ Clonkworks.c4d/2_IO.c4d/C_SENSOROBJ.c4d/Custom.c4d/Script.c
@@ -17,3 +17,12 @@ public func GetDetection(){
   var Object = FindObject2(Find_NoContainer(),Find_Distance(Range),Find_ID(Aidee),Find_Exclude(this),Find_Exclude(InputActive("Except")),Sort_Distance());
   return(Object);
 }
+
+func Detect(){
+  var Type;
+  if(InputActive("ID String")) Type = C4Id(InputActive("ID String"));
+  else Type = RMMG;
+  SetGraphics(nil,this(),Type,1,4);
+
+  return(_inherited());
+}
