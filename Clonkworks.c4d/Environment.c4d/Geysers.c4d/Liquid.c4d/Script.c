c5f4771a31c15561faa2d87a8efb3284facd95e9
Kodenith <watemborskae@gmail.com>
2026-05-09 15:15:40 +0200

    Added the Liquid Geyser

---
+++ Clonkworks.c4d/Environment.c4d/Geysers.c4d/Liquid.c4d/Script.c
@@ -0,0 +1,63 @@
+/*-- Neues Script --*/
+
+#strict 2
+
+local Liquid;
+local Random1;
+
+local Intensity;
+
+func Initialize() {
+  while(GBackSolid()){
+    SetY(GetY()-1);
+    if(GetY() <= 0) break;
+  }
+  SetCategory(C4D_Structure); 
+
+  SetLiquid("Lava");
+  SetRandom(RandomX(120,1500));
+
+  return(1);
+}
+
+func Damage(){
+  if(GetDamage() > 150) RemoveObject();
+}
+
+public func SetLiquid(string Type){
+  if(Type == "DuroLava") SetName(Format("Lava Geyser",Type));
+  else SetName(Format("%s Geyser",Type));
+  return(Liquid = Type);
+}
+
+public func SetRandom(int Am){
+  return(Random1 = Am);
+}
+
+private func Update(){
+  if(GetAction() == "Idle"){
+    if(!Random(Random1)){
+      if(!GBackSolid(GetVertex(0,0),GetVertex(0,1))){
+        Intensity = RandomX(10,25);
+        SetAction("Spew");
+      }else{
+        DoDamage(RandomX(1,15));
+      }
+    }
+  }else{
+      if(!GBackSolid(GetVertex(0,0),GetVertex(0,1))){
+        var Amount = RandomX(10,95);
+        if(!Random(2)) return(0);
+        while(Amount--){
+          var Intensity1 = RandomX(10,25);
+          var Intensity2 = RandomX(30,120);
+          CastPXSX(Liquid,1,RandomX(Intensity1,Intensity),GetVertex(),GetVertex(0,1),-90+RandomX(-20,20));
+        }
+        if(Intensity < Intensity2) Intensity += RandomX(2,10);
+        ShakeViewPort(Intensity/3,this,0,0);
+      }else{
+        SetAction("Idle");
+        DoDamage(RandomX(1,15));
+      }
+  }
+}
